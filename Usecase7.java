de069451fb72e38aeb6bec9a6e3c3d952825ce4b
unknown <nileshdhokrat52586@gmail.com>
2020-09-12 21:44:10 +0530

    Added code to write a class method to compute employee wage

---
+++ Usecase7.java
@@ -0,0 +1,42 @@
+class Usecase7 {
+
+ public static final int FullTime=1, PartTime=2, NoofDays=20, Wage_perhr=20, MaxHrs=100;
+public void employeewage() 
+	{
+	 	int salary=0, totalsalary=0, emphrs=0, totalhrs=0, Days=0;
+                while(totalhrs < MaxHrs && Days < NoofDays)
+                {
+                        int checkno=(int)(Math.floor(Math.random()*10)%3);
+                        Days+=1;
+                        switch(checkno)
+                        {
+                                case FullTime:
+                                        System.out.println("Employee is Full Time....");
+                                        emphrs=8;
+                                break;
+                                case PartTime:
+                                        System.out.println("Employee is part time...!");
+                                        emphrs=4;
+                                break;
+                                default:
+                                        System.out.println("Employee is absent..!");
+                                break;
+                        }
+
+                        salary=Wage_perhr*emphrs;
+                        totalhrs+=emphrs;
+                        System.out.println("salary of employee on "+Days+"th day is "+salary);
+                        totalsalary+=salary;
+
+                }
+                System.out.println("Total salary of Employee...!"+totalsalary);
+	}
+
+public static void main(String args[])
+        {
+		Usecase7 uc=new Usecase7();
+		uc.employeewage();
+
+
+        }
+}
